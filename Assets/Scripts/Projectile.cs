8cbf70d45fe82cf89f0f462d32582eee38d26f78
LoganMurray12 <loganmurray12@live.co.uk>
2025-10-08 21:19:54 +0100

    ADD

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,6 +1,46 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Projectile
+
+
+public class BulletScript : MonoBehaviour
 {
-    
+
+    void Update()
+    {
+
+        GameObject sp;
+
+        Rigidbody body;
+
+        Camera cam;
+
+
+
+        cam = Camera.main;
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            sp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            sp.transform.position = cam.transform.position;
+
+
+
+            body = sp.AddComponent(typeof(Rigidbody)) as Rigidbody;
+
+
+
+            body.AddRelativeForce(cam.transform.forward.normalized * 1000);
+
+        }
+
+    }
+
 }
