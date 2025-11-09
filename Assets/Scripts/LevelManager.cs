7bd75fff4e8b5559f9139c853aaa4da97915525a
LoganMurray12 <loganmurray12@live.co.uk>
2025-11-09 18:14:49 +0000

    ADD
    Started developing the progress bar

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("UI")]
+    public Slider progressBar;      // UI Slider
+    [Header("Progress Settings")]
+    public float currentValue = 0f; // Min
+    public float goalValue = 100f;  // Max
+
+    void Start()
+    {
+        UpdateBar();
+    }
+
+    public void AddProgress(float amount)
+    {
+        currentValue += amount;
+        currentValue = Mathf.Clamp(currentValue, 0, goalValue);
+        UpdateBar();
+    }
+
+    void UpdateBar()
+    {
+        if (progressBar)
+            progressBar.value = currentValue / goalValue;
+    }
+}
